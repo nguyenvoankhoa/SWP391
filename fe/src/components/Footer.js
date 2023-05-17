3bd318518057a9ecdb69792749e40645b16a50e1
mainht <mainhtse171497@fpt.edu.vn>
2023-05-17 22:45:15 +0700

    TM-17/5/2023
    The title "An toàn & chất lượng" is disappeared, I will try to fix it soon

--- fe/src/components/Footer.js
+++ fe/src/components/Footer.js
@@ -1,7 +1,34 @@
 import React from "react";
-
+import './Footer.css'
 const Footer = () => {
-  return <div>Footer</div>;
+return (
+<footer class="footer">
+  <div class="container">
+    <div class="row justify-content-around">
+      <div class="col-md-4 mb-4 address">
+        <h3 class="text-lg font-bold mb-2">ĐỊA CHỈ</h3>
+        <p>Lý Tự Trọng, Bến Nghé, Quận 1,<br />
+        Thành phố Hồ Chí Minh, <br />Việt Nam </p>
+      </div>
+      <div class="col-md-4 mb-4">
+        <h3 class="text-lg font-bold mb-2">LIÊN HỆ</h3>
+        <p>Tổng đài: 1800 1883</p>
+        <p>(8:00 - 21:00, Thứ 2 - CN)</p>
+        <p>Phone: +1 123 456 789</p>
+      </div>
+      <div class="col-md-4 mb-4">
+        <h3 class="text-lg font-bold mb-2">HỖ TRỢ KHÁCH HÀNG</h3>
+        <p>Câu hỏi thường gặp</p>
+        <p>Chính sách bảo mật</p>
+        <p>Quy định và điều khoản</p>
+      </div>
+    </div>
+      <div class="col-md-12">
+        <p class="text-muted text-center">© Copyright 2023 CleanShine</p>
+    </div>
+  </div>
+</footer>
+  );
 };
 
 export default Footer;
--- fe/src/components/WhyUs.js
+++ fe/src/components/WhyUs.js
@@ -1,46 +1,55 @@
 import React from "react";
 import Title from "./Title";
 import Benefit from "./Benefit";
+import 'bootstrap/dist/css/bootstrap.min.css';
 const WhyUs = () => {
   return (
     <>
-      <Title
-        color="#084C61"
-        title="TẠI SAO CHỌN CHÚNG TÔI"
-        fontSize="50px"
-        fontWeight="700"
-      />
-      <div className="row">
+    <Title
+      color="#084C61"
+      title="TẠI SAO CHỌN CHÚNG TÔI"
+      fontSize="50px"
+      fontWeight="700"
+    />
+    <div className="row justify-content-center">
+      <div className="col-md-6">
         <Benefit
-          image="https://cdn-icons-png.flaticon.com/512/1160/1160358.png"
+          image="assets/images/clock.svg"
           title="Đặt lịch nhanh "
           fontSize="50px"
           fontWeight="700"
-          content="Chúng tôi đảm bảo tiếp nhận và xử lí thông tin cũng như xác nhận cung cấp dịch vụ cho khách hàng trong  vòng 60 phút."
-        />
-        <Benefit
-          image="https://cdn-icons-png.flaticon.com/512/1160/1160358.png"
-          title="Đặt lịch nhanh "
-          fontSize="50px"
-          fontWeight="700"
-          content="Chúng tôi đảm bảo tiếp nhận và xử lí thông tin cũng như xác nhận cung cấp dịch vụ cho khách hàng trong  vòng 60 phút."
-        />
-        <Benefit
-          image="https://cdn-icons-png.flaticon.com/512/1160/1160358.png"
-          title="Đặt lịch nhanh "
-          fontSize="50px"
-          fontWeight="700"
-          content="Chúng tôi đảm bảo tiếp nhận và xử lí thông tin cũng như xác nhận cung cấp dịch vụ cho khách hàng trong  vòng 60 phút."
-        />
-        <Benefit
-          image="https://cdn-icons-png.flaticon.com/512/1160/1160358.png"
-          title="Đặt lịch nhanh "
-          fontSize="50px"
-          fontWeight="700"
-          content="Chúng tôi đảm bảo tiếp nhận và xử lí thông tin cũng như xác nhận cung cấp dịch vụ cho khách hàng trong  vòng 60 phút."
+          content="Chúng tôi đảm bảo tiếp nhận và xử lí thông tin cũng như xác nhận cung cấp dịch vụ cho khách hàng trong vòng 60 phút."
         />
       </div>
-    </>
+      <div className="col-md-6">
+        <Benefit
+          image="assets/images/cashless.svg"
+          title="Giá cả rõ ràng"
+          fontSize="50px"
+          fontWeight="700"
+          content="Chúng tôi hiển thị phí dịch vụ và báo giá rõ ràng theo từng sản phẩm dịch vụ yêu cầu."
+        />
+      </div>
+      <div className="col-md-6">
+        <Benefit
+          image="assets/images/cleaning.svg"
+          title="Đa dạng dịch vụ "
+          fontSize="50px"
+          fontWeight="700"
+          content="Chúng tôi cung cấp đa dạng các dịch vụ vệ sinh như tạp vụ vệ sinh thường xuyên, theo giờ, theo ngày. Chúng tôi tự tin có thể đáp ứng mọi yêu cầu của khách hàng."
+        />
+      </div>
+      <div className="col-md-6">
+        <Benefit
+          image="assets/images/safety.svg"
+          title="An toàn & chất lượng "
+          fontSize="0px"
+          fontWeight="700"
+          content="Chúng tôi cam kết đảm bảo quyền lợi khách hàng với chất lượng dịch vụ cao, và sử dụng các loại hóa chất an toàn cho sức khỏe và thân thiện với môi trường."
+        />
+      </div>
+    </div>
+  </>
   );
 };
 
