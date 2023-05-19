bfdbf78973c0c1dc6a1063442ba2c8459eb68a2f
Hoang Anh Luong <vinaast1220@gmail.com>
2023-05-19 20:48:17 +0700

    HA-/19/05/2023

--- fe/src/components/HeadingBar.js
+++ fe/src/components/HeadingBar.js
@@ -1,35 +1,44 @@
-import React, { useState } from "react";
+import React from "react";
+import { FaTimes } from "react-icons/fa"
 import { Link } from "react-router-dom";
 
 const HeadingBar = () => {
+
   return (
-    <nav className="custom-navbar navbar navbar navbar-expand-md">
-      <div className="container-fluid">
-        {/* LOGO */}
-        <a className="navbar-brand logo" href="index.html">
-          <img src="assets/images/LOGO.svg" alt="" />
-        </a>
+    <div className="container-fluid">
 
-        {/* Navigation button when collapse */}
-        <button
-          className="navbar-toggler collapsed nav-button"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarsFurni"
-          aria-controls="navbarsFurni"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
+      {/* LOGO */}
+      <a className="navbar-brand logo" href="index.html">
+        <img src="assets/images/LOGO.svg" alt="" />
+      </a>
+
+      {/* Services */}
+      <nav
+        className="custom-navbar navbar navbar navbar-expand-md heading-bar"
+        type="button"
+      >
+        <div
+          className="navbar-nav"
         >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-
-        {/* Services */}
-        <div className="navbar-nav">
           <ul className="custom-navbar-nav navbar-nav ms-auto mb-2 mb-md-0 service-list">
             <li>
-              <Link className="nav-link otp-service" href="index.html">
+              <Link
+                className="nav-link otp-service service-options"
+                type="button"
+                href="shop.html"
+                style={{ position: 'relative' }}
+              >
                 DỊCH VỤ
                 <div className="arrow"></div>
+                <div className="service-dropdown">
+                  <ul>
+                    <li src>Service 1</li>
+                    <li>Service 2</li>
+                    <li>Service 3</li>
+                    <li>Service 4</li>
+                    <li>Service 5</li>
+                  </ul>
+                </div>
               </Link>
             </li>
             <li>
@@ -54,17 +63,21 @@ const HeadingBar = () => {
             </li>
           </ul>
         </div>
+      </nav>
 
-        {/* Login */}
-        <ul className="custom-navbar-cta navbar-nav mb-2 mb-md-0 ms-5 login">
-          <li>
-            <Link className="nav-link user" href="#">
-              <img src="assets/images/user.svg" alt=""></img>
-            </Link>
-          </li>
-        </ul>
-      </div>
-    </nav>
+      {/* Login */}
+      <ul className="custom-navbar-cta navbar-nav mb-2 mb-md-0 ms-5 login">
+        <li>
+          <Link className="nav-link user" href="#">
+            <img src="assets/images/user.svg" alt=""></img>
+          </Link>
+        </li>
+      </ul>
+      <button className="nav-btn show-btn">
+        <img src="assets/images/menu.svg" alt="" />
+      </button>
+    </div>
+
   );
 };
 
