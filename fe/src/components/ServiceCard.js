50643ded67f21efb56af6236b0a8c6a3585a7eb3
mainht <mainhtse171497@fpt.edu.vn>
2023-05-19 22:33:17 +0700

    TM-19/5/2023

--- fe/src/components/ServiceCard.js
+++ fe/src/components/ServiceCard.js
@@ -4,8 +4,8 @@ import './ServiceCard.css';
 const ServiceCard = () => {
   return (
     <div className='container-fluid'>
-      <div className='row justify-content-evenly'>
-        <div className='col-md-4 mb-4'>
+      <div className='row row-cols-1 row-cols-md-3 g-4'>
+        <div className='col'>
           <div className='card'>
             <div className='card-body'>
               <span className='dot'>
@@ -13,12 +13,12 @@ const ServiceCard = () => {
               </span>
               <h5 className='card-title'>TẠP VỤ VỆ SINH</h5>
               <p>Là đơn vị hàng đầu chuyên cung cấp dịch vụ vệ sinh hàng ngày hoặc theo giờ chuyên nghiệp.</p>
-              <button type="button" class="btn btn-outline-primary">Đặt ngay</button>
+              <button type="button" class="btn btn-outline-secondary">Đặt ngay</button>
             </div>
           </div>
         </div>
        
-        <div className='col-md-4 mb-4'>
+        <div className='col'>
           <div className='card'>
             <div className='card-body'>
               <span className='dot'>
@@ -26,12 +26,12 @@ const ServiceCard = () => {
               </span>
               <h5 className='card-title'>SỬA CHỮA THIẾT BỊ</h5>
               <p>Đội ngũ kỹ thuật được đào tạo bài bản, có kinh nghiệm trong việc nắm bắt và sự lý sự cố.</p>
-              <button type="button" class="btn btn-outline-primary">Đặt ngay</button>
+              <button type="button" class="btn btn-outline-secondary">Đặt ngay</button>
             </div>
           </div>
         </div>
 
-        <div className='col-md-4 mb-4'>
+        <div className='col'>
           <div className='card'>
             <div className='card-body'>
               <span className='dot'>
@@ -39,7 +39,7 @@ const ServiceCard = () => {
               </span>
               <h5 className='card-title'>BẢO HÀNH DỌN DẸP</h5>
               <p>Đảm bảo chính sách bảo hành đối với việc vệ sinh những thiết bị gia dụng thông minh.</p>
-              <button type="button" class="btn btn-outline-primary">Đặt ngay</button>
+              <button type="button" class="btn btn-outline-secondary">Đặt ngay</button>
             </div>
           </div>
         </div>
