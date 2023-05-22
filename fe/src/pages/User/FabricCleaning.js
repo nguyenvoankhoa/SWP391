cf3ca0505d4fd666b1f96024c3b2df3eb1f19f25
mainht <mainhtse171497@fpt.edu.vn>
2023-05-22 13:12:01 +0700

    TM-22/5/2023

--- fe/src/pages/User/FabricCleaning.js
+++ fe/src/pages/User/FabricCleaning.js
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import './FabricCleaning.css';
+import './DatePicker.css'
 
 const FabricCleaning = () => {
-  return <div>FabricCleaning</div>;
+  const [selectedDate, setSelectedDate] = useState(null);
+
+  return (
+    <div className="container-fluid fabric-container">
+      <nav className="navbar navbar-expand-lg fabric-nav">
+        <h1 className="text-center fabric-title">Vệ sinh sofa, nệm, rèm thảm</h1>
+      </nav>
+      <div className="row gy-5 input">
+        <div className="col-md-12">
+          <label className="schedule-input">Lịch làm việc</label>
+          <DatePicker
+            selected={selectedDate}
+            onChange={(date) => setSelectedDate(date)}
+            dateFormat="dd/MM/yyyy"
+            className="fabric-schedule mt-3"
+            placeholderText="Select a date"
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default FabricCleaning;
+
