48a3606a962bc180384d2cabb4316d9548701e3a
mainht <mainhtse171497@fpt.edu.vn>
2023-05-24 07:18:30 +0700

    TM-24/5/2023

--- fe/src/components/ContactInfo.js
+++ fe/src/components/ContactInfo.js
@@ -4,9 +4,9 @@ const ContactInfo = (props) => {
   return (
     <div className="col-lg-4 col-sm-12 mb-4">
       <Card>
-        <div className="row" style={{ minHeight: "100px" }}>
-          <div className="col-4 d-flex justify-content-center align-items-center">
-            <img src={props.image} alt="" className="w-100" />
+        <div className="row" style={{ minHeight: "10px" }}>
+          <div className="col-4 d-flex justify-content-center align-items-center text-align-center">
+            <img src={props.image} alt="" className="w-50" />
           </div>
           <div className="col-8">
             <h5>{props.title}</h5>
