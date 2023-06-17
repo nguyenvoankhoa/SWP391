2d78c1b756b2c4efd8224b6f2afaf8965b725937
ThienAnDy <vaper2212@gmail.com>
2023-06-17 09:05:15 +0700

    Fix add employ

--- fe/src/pages/Admin/EditEmployee.js
+++ fe/src/pages/Admin/EditEmployee.js
@@ -14,9 +14,6 @@ const EditEmployee = () => {
   const editEmployeeHandler = (employee) => {
     setEmployee(employee);
   };
-  const AddEmployeeHandler = (employee) => {
-    setEmployee(employee);
-  };
   return (
     <>
 
@@ -26,15 +23,87 @@ const EditEmployee = () => {
       <div className="row justify-content-center">
         <div className="col-10 ">
           <div className="d-flex flex-row-reverse">
-            <button className="d-flex justify-content-end"
-              onClick={() => AddEmployeeHandler(employee)}
-              startIcon="<AiOutlineUserAdd/>"
-              type="button"
-              class="btn btn-primary"
-              data-bs-toggle="modal"
-              data-bs-target="#exampleModal">
+
+            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
               Thêm Nhân Viên
             </button>
+            <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+              <div class="modal-dialog">
+                <div class="modal-content">
+                  <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="exampleModalLabel">Thêm Nhân Viên</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                  </div>
+                  <div class="modal-body row container d-flex jutify-content-center ">
+                    <div className="col-md-6">
+                      <div className="form-group">
+                        <label htmlFor="fullName">Tên</label>
+                        <input
+                          type="text"
+                          className="form-control"
+                        />
+                      </div>
+                    </div>
+                    <div className="col-md-6">
+                      <div className="form-group">
+                        <label htmlFor="eMail">Email</label>
+                        <input
+                          type="text"
+                          className="form-control"
+                        />
+                      </div>
+                    </div>
+                    <div className="col-md-6">
+                      <div className="form-group">
+                        <label htmlFor="password">
+                          Password
+                        </label>
+                        <input
+                          type="password"
+                          className="form-control"
+                        />
+                      </div>
+                    </div>
+                    <div className="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
+                      <div className="form-group">
+                        <label htmlFor="phone">Số điện thoại</label>
+                        <input
+                          type="text"
+                          className="form-control"
+                          id="phone"
+                        />
+                      </div>
+                    </div>
+                    <div className="col-md-12">
+                      <div className="row">
+                        <div className="form-group col-md-12">
+                          <label htmlFor="workType">Công việc</label>
+                          <select
+                            className="form-control"
+                            id="workType"
+                          >
+                            <option value="Vệ sinh nệm, sofa, thảm">
+                              Vệ sinh nệm, sofa, thảm
+                            </option>
+                            <option value="Vệ sinh máy lạnh">
+                              Vệ sinh máy lạnh
+                            </option>
+                            <option value="Giúp việc theo giờ">
+                              Giúp việc theo giờ
+                            </option>
+                            <option value="Tổng vệ sinh">Tổng vệ sinh</option>
+                          </select>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Save changes</button>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
 
           <Card>
