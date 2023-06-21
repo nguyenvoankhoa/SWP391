fcec756a19234405cc2193a59b3cedd40ce8b800
ThienAnDy <vaper2212@gmail.com>
2023-06-21 07:40:31 +0700

    fix

--- fe/src/pages/User/UserPeriodic.js
+++ fe/src/pages/User/UserPeriodic.js
@@ -19,44 +19,42 @@ const UserPeriodic = (props) => {
       )}
       {data.length > 0 && (
 
-        <div className="row">
-          <div className="table-responsive table-wrapper-scroll-y w-100  my-custom-scrollbar">
-            {" "}
-            <table
-              className="table table-bordered table-striped text-center"
-              style={{ fontSize: "18px", fontWeight: "400" }}
-            >
-              <thead>
-                <tr>
-                  <th scope="col">Dịch vụ</th>
-                  <th scope="col">Loại</th>
-                  <th scope="col">Định kỳ</th>
-                  <th scope="col">Nhân viên</th>
-                  <th scope="col">Giao dịch</th>
-                  <th scope="col">Tổng cộng</th>
+        <div className="table-responsive table-wrapper-scroll-y w-100  my-custom-scrollbar">
+          {" "}
+          <table
+            className="table table-bordered table-striped text-center"
+            style={{ fontSize: "18px", fontWeight: "400" }}
+          >
+            <thead>
+              <tr>
+                <th scope="col">Dịch vụ</th>
+                <th scope="col">Loại</th>
+                <th scope="col">Định kỳ</th>
+                <th scope="col">Nhân viên</th>
+                <th scope="col">Giao dịch</th>
+                <th scope="col">Tổng cộng</th>
+              </tr>
+            </thead>
+            <tbody>
+              {data.map((service) => (
+                <tr key={service.id}>
+                  <td>{service.business.name}</td>
+                  <td>{service.business.type}</td>
+                  <td>{service.frequency}</td>
+                  {service.employee ? (
+                    <td>{service.employee.name}</td>
+                  ) : (
+                    <td>Đang chờ xử lý</td>
+                  )}
+                  <td>{service.payment}</td>
+
+                  <td>{service.total}</td>
                 </tr>
-              </thead>
-              <tbody>
-                {data.map((service) => (
-                  <tr key={service.id}>
-                    <td>{service.business.name}</td>
-                    <td>{service.business.type}</td>
-                    <td>{service.frequency}</td>
-                    {service.employee ? (
-                      <td>{service.employee.name}</td>
-                    ) : (
-                      <td>Đang chờ xử lý</td>
-                    )}
-                    <td>{service.payment}</td>
-
-                    <td>{service.total}</td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          </div>
-
+              ))}
+            </tbody>
+          </table>
         </div>
+
 
       )}
     </>
